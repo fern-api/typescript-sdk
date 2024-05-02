3e6d31d7803de65e7878d897f781918ff5448586
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2024-05-02 19:09:26 +0000

    SDK regeneration

--- src/api/resources/templates/types/SnippetRegistryEntry.ts
+++ src/api/resources/templates/types/SnippetRegistryEntry.ts
@@ -7,5 +7,11 @@ import * as FernInternal from "../../..";
 export interface SnippetRegistryEntry {
     sdk: FernInternal.Sdk;
     endpointId: FernInternal.EndpointIdentifier;
+    /** The default snippet template to use */
     snippetTemplate: FernInternal.VersionedSnippetTemplate;
+    /**
+     * Additional templates to use for this endpoint, for example if you wanted
+     * an async example, you could have { "async": Template(...) }.
+     */
+    additionalTemplates?: Record<string, FernInternal.VersionedSnippetTemplate>;
 }
