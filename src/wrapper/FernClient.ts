472de269e9fbab635e0b6ebc20867f711c64c006
dsinghvi <deep@usebirch.com>
2024-06-05 09:01:06 -0400

    remove more overridden code

--- src/wrapper/FernClient.ts
+++ src/wrapper/FernClient.ts
@@ -1,24 +1,8 @@
-import * as environments from "../environments";
-import * as core from "../core";
 import { FernClient as GeneratedClient } from "../Client";
 import { TemplatesClient } from "./TemplatesClient";
 
-export declare namespace FernClient {
-    interface Options {
-        environment?: core.Supplier<environments.FernEnvironment | string>;
-        token?: core.Supplier<core.BearerToken | undefined>;
-    }
-
-    interface RequestOptions {
-        timeoutInSeconds?: number;
-        maxRetries?: number;
-    }
-}
 
 export class FernClient extends GeneratedClient {
-    constructor(protected readonly _options: FernClient.Options = {}) {
-        super(_options);
-    }
 
     protected _templates: TemplatesClient | undefined;
 
